0e5a200265c6cfcf96b5876243f5a0c4ad95cb84
nazmulislamprofile <74948943+nazmulislamprofile@users.noreply.github.com>
2024-10-05 15:50:45 +0000

    node server created

--- node.js
+++ node.js
@@ -1,1 +1,20 @@
-console.log("hello")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode=201;
+    res.setHeader("MERN-Bacth","NO 7");
+    
+    if (req.url === "/") {
+        res.end("home page");
+    } else if (req.url === "/contact") {
+        res.end("contact page");
+    } else if (req.url === "/profile") {
+        res.end("profile page");
+    } else {
+        res.end("hello node js server");
+    }
+});
+
+server.listen(6060, function() {
+    console.log('server started on port 6060');
+});
